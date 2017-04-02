ef821c0100b89e4937340ee493b97963dc3eea20
Vladimir Timofeev <vovkasm@gmail.com>
2017-04-02 23:37:39 +0300

    Rename test

--- samples/SampleApp/test-borders.js
+++ samples/SampleApp/test-borders.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { ScrollView, StyleSheet, View } from 'react-native'
 import WebImage from 'react-native-web-image'
 
-export default function TestBordersFlexible () {
+export default function TestBordersRectangleMono () {
   const uri = 'https://placeholdit.imgix.net/~text?txtsize=26&txt=100x80&w=100&h=80'
   const bs = {
     borderColor: 'red',
-    borderWidth: 2,
-    borderRadius: 10
+    borderWidth: 1
   }
   return <ScrollView style={s.container}>
     <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs]} /></View>
-    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs, {borderLeftWidth: 30, borderRightWidth: 30}]} /></View>
+    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs, {borderWidth: 20}]} /></View>
+    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs, {borderTopWidth: 1, borderBottomWidth: 1, borderLeftWidth: 20, borderRightWidth: 20}]} /></View>
+    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs, {borderTopWidth: 0, borderBottomWidth: 8, borderLeftWidth: 2, borderRightWidth: 2}]} /></View>    
   </ScrollView>
 }
 
@@ -21,7 +22,8 @@ const s = StyleSheet.create({
   },
   block: {
     borderWidth: 1,
-    borderColor: 'green'
+    borderColor: 'green',
+    margin: 1
   },
   web: {
     flex: 1
