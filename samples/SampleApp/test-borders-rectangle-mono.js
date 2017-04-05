04a6bbe2891d922e7c00d953dc0a18efd7b7ae4d
Vladimir Timofeev <vovkasm@gmail.com>
2017-04-06 01:27:23 +0300

    Improve tests view

--- samples/SampleApp/test-borders-rectangle-mono.js
+++ samples/SampleApp/test-borders-rectangle-mono.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import React, { PropTypes } from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import WebImage from 'react-native-web-image'
 
-export default function TestBordersRectangleMono () {
+function TestItem (props) {
   const uri = 'https://placeholdit.imgix.net/~text?txtsize=26&txt=100x80&w=100&h=80'
-  const bs = {
-    borderColor: 'red',
-    borderWidth: 1
+  const style = {
+    flex: 1,
+    ...props.style
   }
-  return <ScrollView style={s.container}>
-    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs]} /></View>
-    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs, {borderWidth: 20}]} /></View>
-    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs, {borderTopWidth: 1, borderBottomWidth: 1, borderLeftWidth: 20, borderRightWidth: 20}]} /></View>
-    <View style={[s.block, {width: 160, height: 160}]}><WebImage source={{uri}} style={[s.web, bs, {borderTopWidth: 0, borderBottomWidth: 8, borderLeftWidth: 2, borderRightWidth: 2}]} /></View>
+  return <View style={s.test}>
+    <View style={[s.block, {width: 160, height: 160}]}>
+      <WebImage source={{uri}} style={style} />
+    </View>
+    <View style={s.testDescription}>
+      <Text style={s.testDescriptionText}>{JSON.stringify(style, null, ' ')}</Text>
+    </View>
+  </View>
+}
+TestItem.propTypes = {
+  style: PropTypes.object
+}
+
+export default function TestBordersRectangleMono () {
+  const bs = { borderColor: 'red', borderWidth: 1 }
+  return <ScrollView style={s.container} contentContainerStyle={s.content}>
+    <TestItem style={bs} />
+    <TestItem style={{ ...bs, borderWidth: 20 }} />
+    <TestItem style={{ ...bs, borderTopWidth: 1, borderBottomWidth: 1, borderLeftWidth: 20, borderRightWidth: 20 }} />
+    <TestItem style={{ ...bs, borderTopWidth: 0, borderBottomWidth: 8, borderLeftWidth: 2, borderRightWidth: 2 }} />
   </ScrollView>
 }
 
@@ -20,12 +35,24 @@ const s = StyleSheet.create({
   container: {
     flex: 1
   },
+  content: {
+    width: 600
+  },
+  test: {
+    flexDirection: 'row',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ccc',
+    padding: 2
+  },
   block: {
     borderWidth: 1,
-    borderColor: 'green',
-    margin: 1
+    borderColor: 'green'
   },
-  web: {
-    flex: 1
+  testDescription: {
+    flex: 1,
+    padding: 2
+  },
+  testDescriptionText: {
+    fontSize: 8
   }
 })
