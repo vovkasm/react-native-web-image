ce35a6422b1b07d3d4f6af056ad0c4981a14fad5
Vladimir Timofeev <vovkasm@gmail.com>
2017-04-06 19:53:08 +0300

    Add rounded multi-color border tests

---
+++ samples/SampleApp/test-borders-round-color.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
+
+import TestItem from './TestItem'
+
+export default function TestBordersRoundColor () {
+  const bs = {
+    borderTopColor: 'red',
+    borderBottomColor: 'red',
+    borderLeftColor: 'blue',
+    borderRightColor: 'blue',
+    borderRadius: 10,
+    borderWidth: 1
+  }
+  return <ScrollView style={s.container} contentContainerStyle={s.content}>
+    <TestItem style={bs} />
+    <TestItem style={{ ...bs, borderWidth: 20 }} />
+    <TestItem style={{ ...bs, borderTopWidth: 1, borderBottomWidth: 1, borderLeftWidth: 20, borderRightWidth: 20 }} />
+    <TestItem style={{ ...bs, borderTopWidth: 0, borderBottomWidth: 8, borderLeftWidth: 2, borderRightWidth: 2 }} />
+    <TestItem style={{ ...bs, borderWidth: 20, borderTopLeftRadius: 40, borderTopRightRadius: 40, borderBottomLeftRadius: 2, borderBottomRightRadius: 2 }} />
+  </ScrollView>
+}
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  content: {
+    width: 600
+  }
+})
