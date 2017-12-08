611b3812574306e8d5076c3024996133a2b38fda
Vladimir Timofeev <vovkasm@gmail.com>
2017-12-08 23:27:37 +0300

    Perform image loading in didSetProps handler, because in setSource not all props is settled

--- WebImage/WebImage/WebImageView.m
+++ WebImage/WebImage/WebImageView.m
@@ -1,32 +1,41 @@
 #import "WebImageView.h"
 
+@interface WebImageView ()
+
+@property (nonatomic) BOOL needsImageUpdate;
+
+@end
+
 @implementation WebImageView
 
 - (void)setSource:(WebImageSource *)source {
     _source = source;
+    self.needsImageUpdate = YES;
+}
+
+- (void)didSetProps:(NSArray<NSString*>*)changedProps {
+    if (!self.needsImageUpdate) return;
+    self.needsImageUpdate = NO;
     
     typeof(self) __weak wSelf = self;
-    [self sd_setImageWithURL:source.uri completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            // Perform on next cycle, otherwise onWebImageSuccess is nil :-/
-            typeof(self) self = wSelf;
-            if (self == nil) return;
-            
-            if (error) {
-                if (self.onWebImageError) {
-                    self.onWebImageError(@{@"error":error.description, @"uri":imageURL.absoluteString});
-                }
-                return;
+    [self sd_setImageWithURL:self.source.uri completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+        typeof(self) self = wSelf;
+        if (self == nil) return;
+        
+        if (error) {
+            if (self.onWebImageError) {
+                self.onWebImageError(@{@"error":error.description, @"uri":imageURL.absoluteString});
             }
-            if (self.onWebImageSuccess) {
-                NSMutableDictionary* event = [NSMutableDictionary dictionary];
-                event[@"uri"] = imageURL.absoluteString;
-                if (cacheType != SDImageCacheTypeNone) {
-                    event[@"type"] = @"cache";
-                }
-                self.onWebImageSuccess(event);
+            return;
+        }
+        if (self.onWebImageSuccess) {
+            NSMutableDictionary* event = [NSMutableDictionary dictionary];
+            event[@"uri"] = imageURL.absoluteString;
+            if (cacheType != SDImageCacheTypeNone) {
+                event[@"type"] = @"cache";
             }
-        });
+            self.onWebImageSuccess(event);
+        }
     }];
 }
 
