44f4167ae1712d2a364be8ef30101b30688d204f
Vladimir Timofeev <vovkasm@gmail.com>
2017-12-27 00:11:47 +0300

    fisr very simple test

---
+++ android/src/test/java/org/vovkasm/WebImage/RequestListenerTest.java
@@ -0,0 +1,15 @@
+package org.vovkasm.WebImage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RequestListenerTest {
+
+    @Test
+    public void testSample () {
+        int actual = 10;
+        assertEquals(10, actual);
+    }
+
+}
