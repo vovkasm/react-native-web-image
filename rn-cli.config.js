3442f3d3dca7ec79c0384f9533a97fb4f99ccfe1
Vladimir Timofeev <vovkasm@gmail.com>
2018-11-18 11:20:23 +0300

    update configs

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -2,10 +2,12 @@
 const path = require('path')
 
 module.exports = {
-  extraNodeModules: {
-    'react-native-web-image': __dirname
-  },
-  getProjectRoots: function () {
-    return [path.resolve(__dirname, 'samples/SampleApp'), path.resolve(__dirname, '.')]
+  projectRoot: path.resolve(__dirname, 'samples/SampleApp'),
+  watchFolders: [__dirname],
+
+  resolver: {
+    extraNodeModules: {
+      'react-native-web-image': __dirname,
+    }
   }
 }
