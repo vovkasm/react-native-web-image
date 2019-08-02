96b4dca69167d68d30bd3f9501054446c64acec0
Vladimir Timofeev <vovkasm@gmail.com>
2019-08-02 16:43:39 +0300

    upgrade android (step 1)

--- samples/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
+++ samples/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
@@ -1,9 +1,12 @@
 package com.sampleapp;
 
 import android.app.Application;
+import android.util.Log;
 
 import org.vovkasm.WebImage.WebImagePackage;
 
+import com.facebook.react.PackageList;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,10 +26,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new WebImagePackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new WebImagePackage())
+      return packages;
     }
   };
 
